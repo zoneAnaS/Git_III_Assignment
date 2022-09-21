867818c16c3b2253892d1e74e724818bd92b0924
Your Name <sm920149@gmail.com>
2022-09-21 16:58:24 +0530

    i fixed the prime problem

--- primeProblem.js
+++ primeProblem.js
@@ -1,10 +1,10 @@
 
-function primeProblem(n){
-if(n<2){
+function primeProblem(number){
+if(number<2){
 return false;
 }
-for(i=2;i<=n**0.5;i++){
-    if(n%i==0){
+for(i=2;i<=number**1/2;i++){
+    if(number%i==0){
         return false;
     }
 }
